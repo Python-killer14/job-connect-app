a6b2b4f1dec151ce7f7dfeefc59c75841b67d4ab
Robel Tesfay <123911675+Python-killer14@users.noreply.github.com>
2024-07-04 17:54:49 -0400

    DEv: .env file created. ABou to start building the UI part of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+"use client"
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-      <Button>Hello</Button>
+      <h1>EthioJobsConnect</h1>
+      <Button>Click</Button>
     </main>
   );
 }
