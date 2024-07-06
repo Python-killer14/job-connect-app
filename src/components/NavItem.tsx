eee47b8fd3408b1c9c9f3958d13d20365d176aa4
Robel Tesfay <123911675+Python-killer14@users.noreply.github.com>
2024-07-06 14:21:29 -0400

    Deployment test

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,32 +1,31 @@
 "use client"
 import { cn } from '@/lib/utils'
+import { IconName, IconsOptionsType, MenuItemType } from '@/types/navigation/navTypes'
 import { BriefcaseBusiness, Mail, SquareUserRound } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { MenuItemType } from '@/types/navigation/navTypes'
 
 
-const NavItem = ({name, slug, iconName}: MenuItemType) => {
-  const path =usePathname()
-
-  // Check if on this path?
-  const isOnThisPage = path === slug
-
-  const iconOptions = {
-    briefCase: <BriefcaseBusiness className='text-rose-red' size={22} strokeWidth={1.5} />,
-    message: <Mail  className='text-rose-red' size={22} strokeWidth={1.5} />,
-    profile: <SquareUserRound className='text-rose-red' size={23} strokeWidth={1.5}  />
-  }
-
-  
-  return (
-    <li className={cn( isOnThisPage ? "bg-light-rose text-rose-red": "dark:hover:text-black hover:bg-hover-gray", "transition-colors duration-150 rounded-md" )}>
-      <Link href={`${slug}`} className='flex items-center gap-2 px-3 py-2' >
-      {iconOptions[iconName]}
-      {name}
-      </Link>
-      </li>
-  )
+const iconOptions: IconsOptionsType = {
+  briefCase: <BriefcaseBusiness className='text-rose-red' size={22} strokeWidth={1.5} />,
+  message: <Mail className='text-rose-red' size={22} strokeWidth={1.5} />,
+  profile: <SquareUserRound className='text-rose-red' size={23} strokeWidth={1.5} />
 }
 
-export default NavItem
+const NavItem = ({ name, slug, iconName }: MenuItemType) => {
+  const path = usePathname();
+
+  // Check if on this path?
+  const isOnThisPage = path === slug;
+
+  return (
+    <li className={cn(isOnThisPage ? "bg-light-rose text-rose-red" : "dark:hover:text-black hover:bg-hover-gray", "transition-colors duration-150 rounded-md")}>
+      <Link href={`${slug}`} className='flex items-center gap-2 px-3 py-2'>
+        {iconOptions[iconName]}
+        {name}
+      </Link>
+    </li>
+  );
+}
+
+export default NavItem;
--- src/types/navigation/navTypes.ts
+++ src/types/navigation/navTypes.ts
@@ -1,7 +1,12 @@
+export type IconName = 'briefCase' | 'message' | 'profile';
+
+export type IconsOptionsType = {
+  [key in IconName]: JSX.Element;
+}
 
 export interface MenuItemType {
   id?: string,
   name: string,
   slug: string,
-  iconName?: string
+  iconName: IconName
 }
